daf3465f243cdc1fd93b6a4a3e44142e1b0d69b6
eladsadeh <elad.sadeh1@.gmail.com>
2022-02-22 11:25:49 -0500

    rearrange menu and navigation to support navigation based on page

--- components/Nav.js
+++ components/Nav.js
@@ -1,71 +1,21 @@
 import React, { useState } from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
+import NavMenu from './NavMenu';
 import { TiThMenu } from 'react-icons/ti';
 import { IoClose } from 'react-icons/io5';
 
 import styles from '../styles/Nav.module.css';
+import HomeTitle from './HomeTitle';
 
 function Nav(props) {
 	const router = useRouter();
 	const [showMenu, setShowMenu] = useState(false);
-
+    console.log(router);
 	return (
 		<nav className={styles.navbar}>
-			<button
-				className={styles.showButton}
-				onClick={() => {
-					setShowMenu(!showMenu);
-				}}>
-				{showMenu ? <IoClose /> : <TiThMenu />}
-			</button>
-			{showMenu && (
-				<div className={showMenu ? styles.navmenu : styles.slideout}>
-					<ul className={styles.navitems}>
-						<li>
-							<Link href='/crops'>
-								<a>Crops</a>
-							</Link>
-						</li>
-						<li>
-							<Link href='/varieties'>
-								<a>Varieties</a>
-							</Link>
-						</li>
-						<li>
-							<Link href='/seeds'>
-								<a>Seeds</a>
-							</Link>
-						</li>
-						<li>
-							<Link href='/about'>
-								<a>About</a>
-							</Link>
-						</li>
-						<li>
-							<Link href='/'>
-								<a>Home</a>
-							</Link>
-						</li>
-					</ul>
-				</div>
-			)}
-			<h1 className={styles.heading}>
-				<a>
-					<p className={styles.name}>
-						<span>&lt;</span>
-						<span className={styles.blue}>Elad Sadeh </span>
-						<span className={styles.code}>
-							<span className={styles.green}>whatIDo</span>
-							<span className={styles.red}>=</span>
-							<span className={styles.purple}>&#123;</span>
-							<span className={styles.orange}>Software Engineering</span>
-							<span className={styles.purple}>&#125;</span>
-						</span>{' '}
-						/&gt;
-					</p>
-				</a>
-			</h1>
+			<NavMenu />
+			<HomeTitle />
 		</nav>
 	);
 }
---
+++ components/NavMenu.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import { TiThMenu } from 'react-icons/ti';
+import { IoClose } from 'react-icons/io5';
+
+import styles from '../styles/NavMenu.module.css';
+
+function NavMenu(props) {
+	const router = useRouter();
+	const [showMenu, setShowMenu] = useState(false);
+
+	return (
+		<>
+			<button
+				className={styles.showButton}
+				onClick={() => {
+					setShowMenu(!showMenu);
+				}}>
+				{showMenu ? <IoClose /> : <TiThMenu />}
+			</button>
+			{showMenu && (
+				<div className={showMenu ? styles.navmenu : styles.slideout}>
+					<ul className={styles.navitems}>
+						<li className={styles.link}>
+							<Link href='/work'>
+								<a>Work Experience</a>
+							</Link>
+						</li>
+						<li className={styles.link}>
+							<Link href='/projects'>
+								<a>Projects</a>
+							</Link>
+						</li>
+						<li className={styles.link}>
+							<Link href='/education'>
+								<a>Education</a>
+							</Link>
+						</li>
+						<li className={styles.link}>
+							<Link href='/background'>
+								<a>Everything Else</a>
+							</Link>
+						</li>
+						<li className={styles.link}>
+							<Link href='/'>
+								<a>Home</a>
+							</Link>
+						</li>
+					</ul>
+				</div>
+			)}
+		</>
+	);
+}
+
+export default NavMenu;

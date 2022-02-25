5081d758d222bb1e0d2ca1692093412d42184b5a
eladsadeh <elad.sadeh1@.gmail.com>
2022-02-25 17:37:57 -0500

    fine tune font sizing in accordion

--- components/AccordionItem.js
+++ components/AccordionItem.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import { FiChevronLeft, FiChevronDown } from 'react-icons/fi';
 import styles from '../styles/accordionItem.module.css';
+import utilStyles from '../styles/utils.module.css'
 
 function AccordionItem({ content, toggleContent, idx, showContent }) {
 	return (
 		<div className={styles.container}>
-			<div className={showContent? `${styles.active} ${styles.title}`: styles.title} onClick={() => toggleContent(idx)}>
+			<div
+				className={
+					showContent ? `${styles.active} ${styles.title}` : styles.title
+				}
+				onClick={() => toggleContent(idx)}>
 				<h2>{content.title}</h2>
-				{showContent ? <FiChevronDown className={styles.icon}/> : <FiChevronLeft />}
+				{showContent ? (
+					<FiChevronDown className={styles.icon} />
+				) : (
+					<FiChevronLeft />
+				)}
 			</div>
 			{showContent && (
 				<div className={styles.content}>
 					<div className={styles.description}>{content.description}</div>
-					<ul className={styles.bullets}>
+					<ul className={utilStyles.bullets}>
 						{content.bullets.map((bullet, idx) => {
-							return <li key={idx}><p>{bullet}</p></li>;
+							return (
+								<li key={idx}>
+									<p>{bullet}</p>
+								</li>
+							);
 						})}
 					</ul>
 				</div>

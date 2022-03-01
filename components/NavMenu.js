6348d0c945a153f9c3e8917b0b8e70aee6ffeb7c
eladsadeh <elad.sadeh1@.gmail.com>
2022-03-01 14:15:50 -0500

    replace menu icon and modify transition

--- components/NavMenu.js
+++ components/NavMenu.js
@@ -12,44 +12,51 @@ function NavMenu(props) {
 
 	return (
 		<>
-			<button 
+			<div
+				className={styles.menuIcon}
+				onClick={() => {
+					setShowMenu(!showMenu);
+				}}>
+				<div className={styles.menuline}></div>
+				<div className={styles.menuline}></div>
+				<div className={styles.menuline}></div>
+			</div>
+			{/* <button
 				className={styles.showButton}
 				onClick={() => {
 					setShowMenu(!showMenu);
 				}}>
 				{showMenu ? <IoClose /> : <TiThMenu />}
-			</button>
-			{showMenu && (
-				<div className={showMenu ? styles.navmenu : styles.slideout}>
-					<ul className={styles.navitems}>
-						<li className={styles.link}>
-							<Link href='/work'>
-								<a>Work Experience</a>
-							</Link>
-						</li>
-						<li className={styles.link}>
-							<Link href='/projects'>
-								<a>Projects</a>
-							</Link>
-						</li>
-						<li className={styles.link}>
-							<Link href='/education'>
-								<a>Education</a>
-							</Link>
-						</li>
-						<li className={styles.link}>
-							<Link href='/about'>
-								<a>Everything Else</a>
-							</Link>
-						</li>
-						<li className={styles.link}>
-							<Link href='/'>
-								<a>Home</a>
-							</Link>
-						</li>
-					</ul>
-				</div>
-			)}
+			</button> */}
+			<div className={showMenu ? styles.navmenu : styles.hideMenu}>
+				<ul className={styles.navitems}>
+					<li className={styles.link}>
+						<Link href='/work'>
+							<a>Work Experience</a>
+						</Link>
+					</li>
+					<li className={styles.link}>
+						<Link href='/projects'>
+							<a>Projects</a>
+						</Link>
+					</li>
+					<li className={styles.link}>
+						<Link href='/education'>
+							<a>Education</a>
+						</Link>
+					</li>
+					<li className={styles.link}>
+						<Link href='/about'>
+							<a>Everything Else</a>
+						</Link>
+					</li>
+					<li className={styles.link}>
+						<Link href='/'>
+							<a>Home</a>
+						</Link>
+					</li>
+				</ul>
+			</div>
 		</>
 	);
 }
